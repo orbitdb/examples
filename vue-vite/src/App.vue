04cae9259cdeaa25165214c991683d097acf8bda
Hayden Young <haydenyoung@wijiti.com>
2024-01-29 18:09:56 +0000

    refactor: Use Helia.

--- vue-vite/src/App.vue
+++ vue-vite/src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { create } from 'ipfs-core';
-import { createOrbitDB } from '@orbitdb/core';
+import { ref, onMounted } from 'vue'
+import { createHelia, libp2pDefaults } from 'helia'
+import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import { createOrbitDB } from '@orbitdb/core'
 
 const pElementRef = ref(null)
 
 onMounted(async () => {
-  const ipfs = await create({repo: 'ok' + Math.random()});
-  const orbitdb = await createOrbitDB({ ipfs });
+  const libp2pOptions = libp2pDefaults()
+  libp2pOptions.services.pubsub = gossipsub()
+  const ipfs = await createHelia({ libp2p: libp2pOptions })
+  const orbitdb = await createOrbitDB({ ipfs })
   const db = await orbitdb.open('my-db')
-  pElementRef.value.textContent = db.address;
-  await db.close();
-  await ipfs.stop();
+  pElementRef.value.textContent = db.address
+  await db.close()
+  await ipfs.stop()
 })
 </script>
 

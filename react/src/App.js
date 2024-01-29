926742bcc2ca902eda96cf8e2a10a32de2770187
Hayden Young <haydenyoung@wijiti.com>
2024-01-29 18:06:16 +0000

    fix: Linting.

--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import { createHelia, libp2pDefaults } from 'helia';
+import './App.css'
+import { useState, useEffect } from 'react'
+import { createHelia, libp2pDefaults } from 'helia'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
-import { createOrbitDB } from '@orbitdb/core';
+import { createOrbitDB } from '@orbitdb/core'
 
 function App() {
-  const [address, setAddress] = useState('');
-  let loading = false;
+  const [address, setAddress] = useState('')
+  let loading = false
   
   useEffect(() => {
     async function loadDB() {
       const libp2pOptions = libp2pDefaults()
       libp2pOptions.services.pubsub = gossipsub()
       const ipfs = await createHelia({ libp2p: libp2pOptions })
-      const orbitdb = await createOrbitDB({ ipfs });
+      const orbitdb = await createOrbitDB({ ipfs })
       const db = await orbitdb.open('my-db')
-      setAddress(db.address);
+      setAddress(db.address)
       await db.close()
       await ipfs.stop()
     }
